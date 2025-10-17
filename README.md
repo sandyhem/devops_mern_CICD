c9e3ad7b57ead6ca77e80f4c8fb7ec09e5088e3e
sandyhem <santhoshkowsalya2004@gmail.com>
2025-10-17 17:44:55 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# devops_mern_CICD
+This is a demo project for learning github actions, devops and AWS deployment.
---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const {server,app} = require('../index');
+const { default: mongoose } = require('mongoose');
+
+// test suite with only one test case
+describe('GET /api/tasks', () => {
+    it('should return 200 ok', async () => {
+        const res = await request(app).get('/api/tasks');
+        expect(res.status).toBe(200);
+   });
+    it('should return object ok', async () => {
+        const res = await request(app).get('/api/tasks');
+        // expect(Array.isArray(res.body)).toBe(true);
+        expect(typeof res.body).toBe('object');
+        expect(res.body).toHaveProperty('tasks');
+    });
+});
+
+
+//you need to close all connections here
+afterAll(async ()=>{
+    await mongoose.connection.close();
+    await server.close();
+})
